0e07e753bb7f9e08922d28c22195b86d97f81d20
Moses Sangobiyi <hello@sangoandco.com>
2025-03-21 13:37:59 +0000

    FEAT: code cleanup BREAKING CHANGE: rename task 1 directory BREAKING CHANGE: move gitignore file to project root & remove old files BREAKING CHANGE: remove code for previous solution

--- node-app/src/utils/logger.ts
+++
@@ -1,6 +0,0 @@
-import chalk from "chalk";
-
-export const logger = {
-    info: (...args: any[]) => console.log(chalk.green("[INFO]:"), ...args),
-    error: (...args: any[]) => console.error(chalk.red("[ERROR]:"), ...args),
-};
